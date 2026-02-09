083eda5085bbadf2564c495188c7c7e39d28e6e8
PavloKodzhebash <pavlo.kodzhebash@bellevuecollege.edu>
2026-02-08 22:56:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function openModal(img) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const caption = document.getElementById("modal-caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    caption.innerHTML = img.title;
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+function addToCart(itemName) {
+    alert(itemName + " has been added to your cart!");
+}
 
